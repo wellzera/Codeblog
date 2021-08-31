65b247ec7a7233ed3424cd7d01405802f74bd49a
wellzera <WeLLNaNo0>
2021-08-31 19:39:49 -0300

    First commit

---
+++ src/main/java/com/spring/blog/service/BlogService.java
@@ -0,0 +1,14 @@
+package com.spring.blog.service;
+
+import com.spring.blog.model.Post;
+
+import java.util.List;
+
+public interface BlogService {
+    List<Post> findAll();
+    Post findById(long id);
+    Post save(Post post);
+    void delete(long id);
+
+
+}
